014d05406579552237779460bd8124d2c9cb9b4b
dezlymacauley <dezlymacauley@proton.me>
2026-02-06 07:25:07 +0200

    Added more guides

---
+++ src/routes/02-size/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import favicon from "$lib/assets/svelte_logo.svg";
+  import "./page.css";
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<!--
+  `size` allows you to set the height and width of an element at the same
+  time. E.g. `size-20` is the same as `h-20 w-20`
+
+  This can be quite useful for creating squares
+-->
+<div class="bg-amber-300 size-20">
+
+</div>
+
+<!--
+  This is the long version
+-->
+<div class="bg-pink-500 h-20 w-20">
+
+</div>
--- src/routes/04-images/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  import favicon from "$lib/assets/svelte_logo.svg";
-  import "./page.css";
-</script>
-
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
